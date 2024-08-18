57b85f0bf74aed52543b0dcd24bf0ce4e0aae2d2
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-08-18 21:53:31 +0100

    mongo cluster initialized and connected

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,27 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req,res)=>{
     console.log('user has been registered !');
     
-    const {name,email,password} = req.body;
+    const {username,email,password} = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10)
 
-    console.log(hashedPassword);
+    // console.log(hashedPassword);
+
+    const newUser = await prisma.user.create({
+        data : {
+            username,
+            email,
+            password:hashedPassword,
+        },
+    });
     
-}
+    console.log(newUser);
+    
+
+};
 
 export const login = (req,res) => {}
 
