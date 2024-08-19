d940052097bc83546befe849472f9a379a76da57
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-08-19 12:00:13 +0300

    user login authentication

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import postRouter from './routes/post.route.js';
 import auth from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/post", postRouter);
 app.use("/api/auth",auth);
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,15 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+import { json } from "express";
 
 export const register = async (req,res)=>{
+
     console.log('user has been registered !');
     
     const {username,email,password} = req.body;
+
+    try{
 
     const hashedPassword = await bcrypt.hash(password, 10)
 
@@ -17,12 +22,65 @@ export const register = async (req,res)=>{
             password:hashedPassword,
         },
     });
-    
+
     console.log(newUser);
     
+    res.status(201).json({message:'user registered'});
+    }catch(err){
+
+    console.log(err);
+    res.status(500).json({message:"Failed to register a user"});
+
+    }
 
 };
 
-export const login = (req,res) => {}
+export const login = async (req,res) => {
+
+    const {username, password} = req.body;
+
+    try{
+    // CHECK WHETHER USER EXISTS
+
+    const user = await prisma.user.findUnique({
+        where: { username },
+    });
+
+    if (!user) return res.status(401).json({message: "Invalid Credentials!"})
+
+    //CHECK IF PASSWORD IS CORRECT
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) return res.status(401).json({message:'Invalid Credentials!'})
+
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success");
+
+    //GENERATE COOKIE TOKEN TO SEND TO USER
+
+    const token = jwt.sign({
+        id:user.id
+    }, process.env.JWT_SECRET_KEY,
+       {expiresIn: age} 
+    );
+
+
+    const age = 100 * 60 * 60 *24 * 7;
+    res
+    .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+    })
+    .status(200)
+    .json({message: "Login successful"});
+
+    }catch(err){
+    
+    console.log(err);
+    // res.status(500).json({message:"Failed to login a user"});
+
+    }
+
+}
 
 export const logout = (req,res) => {}
