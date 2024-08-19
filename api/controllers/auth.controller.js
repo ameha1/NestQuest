d300d953ec5a7cdf5875d599acfdf0b2c719dd67
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-08-19 12:18:39 +0300

    logout authentication

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -82,4 +82,7 @@ export const login = async (req,res) => {
 
 }
 
-export const logout = (req,res) => {}
+export const logout = (req,res) => {
+
+    res.clearCookie("token").status(200).json({message:"Logout Successful"})
+}
