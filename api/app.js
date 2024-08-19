dd9c534fcbaea21af076a30680fdae81f987f915
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-08-19 20:59:22 +0100

    frontend client side

--- api/app.js
+++ api/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import postRouter from './routes/post.route.js';
 import auth from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin:process.env.CLIENT_URL, credentials:true}))
 
 app.use("/api/post", postRouter);
 app.use("/api/auth",auth);
